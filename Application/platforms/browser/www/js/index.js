b427f6236f413306a37b854c1de406302be4444c
rhysommer <sommertech4@gmail.com>
2018-02-18 15:48:25 +1030

    Fix
    Fixed html editing

--- Application/platforms/browser/www/js/index.js
+++ Application/platforms/browser/www/js/index.js
@@ -26,7 +26,7 @@ var app = {
     // Bind any events that are required on startup. Common events are:
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
-        document.addEventListener('Testing', this.onDeviceReady, false);
+        document.addEventListener('DEVICES', this.onDeviceReady, false);
     },
     // deviceready Event Handler
     //
